433c0930bf8ab7edc8e32dcd74bcd95bc25e4fb5
Katherine Hambley <superdesigngirl@mac.com>
2020-04-19 07:27:59 -0500

    step 4 refactored app to make more modular

---
+++ app/app.module.js
@@ -0,0 +1,5 @@
+//Define the `phonecatApp` module'
+angular.module('phonecatApp', [
+    'phoneList'
+]);
+
--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,13 +1,6 @@
 // Register `phoneList` component, along with its associated controller and template
-angular.module('phonecatApp').component('phoneList', {
-    template:
-    '<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
-    '<ul>' +
-        '<li ng-repeat="phone in $ctrl.phones">' +
-            '<span>{{phone.name}}</span>' +
-            '<p>{{phone.snippet}}</p>' +
-        '</li>' +
-    '</ul>',
+angular.module('phoneList').component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
     controller: function PhoneListController(){
         this.phones = [
             {
